0cb4589c7664d11fa324a2dee8a37c65d9af6618
Selim Altayev <selim.altayev@gmail.com>
2024-12-31 04:35:36 +0100

    Added body falling emojo generator, added new email

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import "./App.scss"
+import "./App.scss";
 import Form from "./components/Form/Form";
+import EmojiGenerator from "./components/EmojiGenerator/EmojiGenerator";
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <EmojiGenerator
+        emojis={["❤️", "❄️", "❄️", "❄️", "❄️", "🌟", "🎉"]}
+        count={100}
+      />
     </div>
   );
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -53,7 +53,7 @@ function Form() {
         try {
           const result = await emailjs.send(
             import.meta.env.VITE_EMAILJS_SERVICE_ID,
-            import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+            emailData.template_id,
             emailData,
             import.meta.env.VITE_EMAILJS_PUBLIC_KEY
           );
